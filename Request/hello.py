9fc22914eef061d00122413d0a1072fa1f84bacc
Fernando Castañeda <108019674+fercho11@users.noreply.github.com>
2022-11-15 10:36:38 -0600

    Update hello.py

--- Request/hello.py
+++ Request/hello.py
@@ -1,4 +1,3 @@
-from pydoc import pager
 from flask import Flask, render_template, request
 import requests
 
@@ -6,20 +5,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    #return "<h1>Hello, World!</h1>"
+    # return "<p>Hello, World!</p>"
     return render_template('home.html')
-    
+
 @app.route("/hola/<name>")
 def hello(name):
     return render_template('hola.html', name=name)
-
-@app.route('/rick/<page>')
+    
+@app.route("/rick/<page>")
 def rick_and_morty(page=1):
 
     url = f'https://rickandmortyapi.com/api/character/?page={page}'
 
-    payload= {}
-    headers= {}
+    payload = {}
+    headers = {}
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
@@ -28,17 +27,17 @@ def rick_and_morty(page=1):
     personajes = respuesta_json['results']
 
     next = int(page) + 1
-    prev = int(page) - 1
+    prev = int(page) -1
 
     return render_template('rick.html', personajes=personajes, prev=prev, next=next)
-
-@app.route('/search', methods=['GET', 'POST'])
+    
+@app.route("/search", methods=['GET', 'POST'])
 def search():
-    search = request.form['search']
+    search =request.form['search']
     if len(search) > 0:
         url = f'https://rickandmortyapi.com/api/character/?name={search}'
-        payload= {}
-        headers= {}
+        payload = {}
+        headers = {}
         response = requests.request("GET", url, headers=headers, data=payload)
         respuesta_json = response.json()
         info = respuesta_json['info']
@@ -49,11 +48,11 @@ def search():
         else:
             next = 1
             prev = 1
-            return render_template('rick.html', personajes=personajes, prev=prev, next=next)    
-        else:
-            return 'No se pudo hacer la busqueda'
+                    
+        return render_template('rick.html', personajes=personajes, prev=prev, next=next)
 
-    
+    else:
+        return 'No se puede hacer la busqueda'
 
 if __name__ == '__main__':
     app.run(debug=True)
